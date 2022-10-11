2b02d8733d31cad03f0aad1f06fc9d66a5c7e94c
hyphenc <46054695+hyphenc@users.noreply.github.com>
2022-10-11 22:58:09 +0200

    add sqlite schema

---
+++ schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS bookmarks;
+
+CREATE TABLE bookmarks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title TEXT NOT NULL,
+    currentlink TEXT NOT NULL,
+    origlink TEXT NOT NULL,
+    archivelink TEXT NOT NULL,
+    domain TEXT NOT NULL,
+    description TEXT NOT NULL,
+    note TEXT NOT NULL,
+    tags TEXT NOT NULL
+);
+
