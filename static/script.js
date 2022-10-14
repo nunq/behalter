bf46e51908493d418f94aa91e638b3393ce4c150
hyphenc <46054695+hyphenc@users.noreply.github.com>
2022-10-14 23:41:41 +0200

    automatically fetch link info on focus loss

--- static/script.js
+++ static/script.js
@@ -1,23 +1,38 @@
 function send() {
   l = document.getElementById("new-link");
   t = document.getElementById("new-title");
-  d = document.getElementById("new-description").value;
+  d = document.getElementById("new-detail").value;
   n = document.getElementById("new-note").value;
   ta = document.getElementById("new-tags").value;
-  l.reportValidity();
-  t.reportValidity();
 
-  fetch("/api/add?link="+encodeURIComponent(l.value)+"&note="+encodeURIComponent(n)+"&tags="+encodeURIComponent(ta)+"&description="+encodeURIComponent(d)+"&title="+encodeURIComponent(t.value))
+  if(l.value == "" || t.value == "") {
+    l.reportValidity();
+    t.reportValidity();
+    return
+  }
+
+  fetch("/api/add?link="+encodeURIComponent(l.value)+"&note="+encodeURIComponent(n)+"&tags="+encodeURIComponent(ta)+"&detail="+encodeURIComponent(d)+"&title="+encodeURIComponent(t.value))
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
 
+function setlinkinfo(data) {
+  document.getElementById("new-title").value = data["title"];
+  document.getElementById("new-detail").value = data["detail"];
+}
+
 function getlinkinfo() {
   l = document.getElementById("new-link");
-  t = document.getElementById("new-title");
-  l.reportValidity();
+
+  if(l.value == "") {
+    l.reportValidity();
+    return
+  }
+
+  document.getElementById("new-title").placeholder = "fetching title...";
+  document.getElementById("new-detail").placeholder = "fetching detail...";
 
   fetch("/api/linkinfo?link="+encodeURIComponent(l.value))
     .then((response) => response.json())
-    .then((data) => t.value = data["title"]);
+    .then((data) => setlinkinfo(data));
 }
