fa0fa42f0dba9bdfb42c1c0daec0f3010da8989c
hyphenc <46054695+hyphenc@users.noreply.github.com>
2022-10-17 00:06:02 +0200

    revamp sql schema

--- schema.sql
+++ schema.sql
@@ -1,15 +1,21 @@
 DROP TABLE IF EXISTS bookmarks;
+DROP TABLE IF EXISTS tags;
 
 CREATE TABLE bookmarks (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created INTEGER NOT NULL DEFAULT (unixepoch()),
+    deleted BOOLEAN NOT NULL DEFAULT FALSE,
     title TEXT NOT NULL,
+    detail TEXT NOT NULL DEFAULT "",
     currentlink TEXT NOT NULL,
     origlink TEXT NOT NULL,
     archivelink TEXT NOT NULL,
     domain TEXT NOT NULL,
-    description TEXT NOT NULL,
     note TEXT NOT NULL,
     tags TEXT NOT NULL
 );
 
+CREATE TABLE tags (
+  name TEXT PRIMARY KEY NOT NULL,
+  usage INTEGER NOT NULL
+);
