3421649049bb26c314c701ce92845cf369142644
hyphenc <46054695+nunq@users.noreply.github.com>
2024-05-07 21:04:44 +0200

    rework __init__.py

--- behalter/__init__.py
+++ behalter/__init__.py
@@ -1,28 +1,8 @@
 #!/usr/bin/env python3
-import atexit
-import os
-
-from datetime import datetime
-
-from flask import Flask#, render_template, request, send_from_directory
-
-# -----------------------------------
-""" # TODO
-def shutdown():
-    conn.commit()
-    conn.close()
-
-atexit.register(shutdown)
-"""
+from flask import Flask
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bm.db"
+
 import behalter.views # pylint: disable=wrong-import-position
-
-@app.template_filter()
-def unix_to_date(timestamp):
-    return datetime.utcfromtimestamp(timestamp).strftime("%d-%m-%Y")
-
-
-if __name__ == "__main__":
-    app.run()
