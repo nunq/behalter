e1e88c07f2021f2836c991e4b3478cfa560a9674
hyphenc <46054695+nunq@users.noreply.github.com>
2024-05-08 16:24:05 +0200

    add /api/bm/add route, /api/bm/linkinfo route

--- behalter/views.py
+++ behalter/views.py
@@ -1,24 +1,53 @@
 #!/usr/bin/env python3
-import json
-import re
-import sqlite3
-import urllib.request
+"""flask views"""
 from pathlib import Path
+from json import dumps
 
-from bs4 import BeautifulSoup
 from flask import render_template, request, send_from_directory
 
-from behalter import app, db
-from behalter.util import unix_to_date
+from behalter import app, database
+from behalter.util import *
 
 
 @app.route("/")
 def index():
-    bm = db.get_all_bookmarks()
+    bm = database.get_all_bookmarks()
     return render_template("index.html", bookmarks=bm)
 
 
 @app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(Path(app.root_path) / "static", "favicon.ico", mimetype="image/vnd.microsoft.icon")
+    return send_from_directory(
+        Path(app.root_path) / "static",
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
 
+
+@app.route("/api/bm/add")
+def add_bookmark():
+    ra = request.args
+    title = ra.get("title")
+    link = ra.get("link")
+    detail = ra.get("detail")
+    note = ra.get("note")
+    tags = ra.get("tags")
+
+    if tags != "":
+        created_bm = database.create_bookmark(title, link, detail, note, tags)
+    else:
+        created_bm = database.create_bookmark(title, link, detail, note)
+
+    # TODO handle duplicates
+    # TODO redirect to bookmark if duplicate detected
+    # TODO implement search?id=<id> for that
+    return dumps(
+        {"result": "success", "bmhtml": render_template("bookmark.html", bm=created_bm)}
+    )
+
+
+@app.route("/api/bm/linkinfo")
+def link_info():
+    ra = request.args
+    link = ra.get("link")
+    return fetch_link_info(link)
