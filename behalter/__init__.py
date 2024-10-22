13daef4cb83a8ce856dc870cf0945b58f64a825b
hyphenc <46054695+nunq@users.noreply.github.com>
2024-10-22 22:19:10 +0200

    feat: add json config file for flask instance

--- behalter/__init__.py
+++ behalter/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """flask main file"""
+import json
+
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_file("config.json", load=json.load)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bm.db"
-
-import behalter.views  # pylint: disable=wrong-import-position
+import behalter.views # pylint: disable=wrong-import-position
